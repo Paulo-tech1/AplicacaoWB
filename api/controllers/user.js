24e146dcbcafd16d5859a820867fea0f084405af
Paulo Costa <pauloroberto.redes@gmail.com>
2023-09-05 23:54:04 -0300

    Projetos TCC
    Trabalhando no código

---
+++ api/controllers/user.js
@@ -0,0 +1,40 @@
+import { db } from "../db.js";
+
+export const getUsers = (_, res) => {
+  const q = "SELECT * FROM usuarios";
+
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data);
+  });
+};
+
+export const addUser = (req, res) => {
+  const q =
+    "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_atendimento`) VALUES(?)";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.data_atendimento,
+  ];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário criado com sucesso.");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q =
+    "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.data_nascimento,
+  ];
